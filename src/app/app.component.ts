cf68815c4bf7f46a62009a0eb9e1bca90d3a8377
Paul Cox <paul.cox@legalbricks.co.uk>
2023-06-30 08:29:17 +0100

    Add basic Onfido usage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
 
+import { init as onfidoInit, SdkHandle as onfidoSdkHandle } from 'onfido-sdk-ui';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
+  private onfido: onfidoSdkHandle;
+
   title = 'of-example';
+
+  constructor() {
+
+    this.onfido = onfidoInit({
+      token: 'token',
+      containerId: 'onfido-mount',
+      onComplete: response => {
+        console.log('onComplete: ', response);
+      },
+      onError: data => {
+        console.log('onError: ', data);
+      },
+      onUserExit: _ => {
+        console.log('onUserExit');
+      },
+      steps: [
+        'welcome',
+        {
+          type: 'document',
+          options: {
+            documentTypes: {
+              passport: true,
+              driving_licence: true
+            },
+            forceCrossDevice: true
+          }
+        },
+        {
+          type: 'face',
+          options: {
+            requestedVariant: 'motion',
+            motionFallbackVariant: 'video',
+          }
+        },
+        'complete'
+      ]
+    });
+  }
 }
